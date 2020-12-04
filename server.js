1b3c4027c62f46e15ff7938f189b4ce0a12fa1d3
simonesafira2007 <simone.safira2007@gmail.com>
2020-12-04 20:33:06 -0300

    alterações

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
-const port = 3000;
+const port = 3000
 
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
-});
+app.listen(process.env.PORT || port, () => {
+  console.log(`Servidor está rodando na porta ${port}`)
+}) 
+
